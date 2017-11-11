822d7873c03465f2cab37ef4d00ff9e48f20e453
David Clark <davidclark314@gmail.com>
2017-11-11 15:18:43 -0700

    Just some initial syntax clean-up so the code is more like what I'm used to.

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -1,9 +1,9 @@
 /*
- * helper_functions.h
- * Some helper functions for the 2D particle filter.
- *  Created on: Dec 13, 2016
- *      Author: Tiffany Huang
- */
+* helper_functions.h
+* Some helper functions for the 2D particle filter.
+*  Created on: Dec 13, 2016
+*      Author: Tiffany Huang
+*/
 
 #ifndef HELPER_FUNCTIONS_H_
 #define HELPER_FUNCTIONS_H_
@@ -20,45 +20,42 @@ const double M_PI = 3.14159265358979323846;
 #endif
 
 /*
- * Struct representing one position/control measurement.
- */
+* Struct representing one position/control measurement.
+*/
 struct control_s {
-	
 	double velocity;	// Velocity [m/s]
-	double yawrate;		// Yaw rate [rad/s]
+	double yaw_rate;		// Yaw rate [rad/s]
 };
 
 /*
- * Struct representing one ground truth position.
- */
+* Struct representing one ground truth position.
+*/
 struct ground_truth {
-	
 	double x;		// Global vehicle x position [m]
 	double y;		// Global vehicle y position
 	double theta;	// Global vehicle yaw [rad]
 };
 
 /*
- * Struct representing one landmark observation measurement.
- */
+* Struct representing one landmark observation measurement.
+*/
 struct LandmarkObs {
-	
 	int id;				// Id of matching landmark in the map.
 	double x;			// Local (vehicle coordinates) x position of landmark observation [m]
 	double y;			// Local (vehicle coordinates) y position of landmark observation [m]
 };
 
 /*
- * Computes the Euclidean distance between two 2D points.
- * @param (x1,y1) x and y coordinates of first point
- * @param (x2,y2) x and y coordinates of second point
- * @output Euclidean distance between two 2D points
- */
+* Computes the Euclidean distance between two 2D points.
+* @param (x1,y1) x and y coordinates of first point
+* @param (x2,y2) x and y coordinates of second point
+* @output Euclidean distance between two 2D points
+*/
 inline double dist(double x1, double y1, double x2, double y2) {
 	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-inline double * getError(double gt_x, double gt_y, double gt_theta, double pf_x, double pf_y, double pf_theta) {
+inline double* getError(double gt_x, double gt_y, double gt_theta, double pf_x, double pf_y, double pf_theta) {
 	static double error[3];
 	error[0] = fabs(pf_x - gt_x);
 	error[1] = fabs(pf_y - gt_y);
@@ -71,24 +68,22 @@ inline double * getError(double gt_x, double gt_y, double gt_theta, double pf_x,
 }
 
 /* Reads map data from a file.
- * @param filename Name of file containing map data.
- * @output True if opening and reading file was successful
- */
+* @param filename Name of file containing map data.
+* @output True if opening and reading file was successful
+*/
 inline bool read_map_data(std::string filename, Map& map) {
-
 	// Get file of map:
 	std::ifstream in_file_map(filename.c_str(),std::ifstream::in);
 	// Return if we can't open the file.
 	if (!in_file_map) {
 		return false;
 	}
-	
+
 	// Declare single line of map file:
 	std::string line_map;
 
 	// Run over each single line:
 	while(getline(in_file_map, line_map)){
-
 		std::istringstream iss_map(line_map);
 
 		// Declare landmark values and ID:
@@ -111,15 +106,15 @@ inline bool read_map_data(std::string filename, Map& map) {
 		// Add to landmark list of map:
 		map.landmark_list.push_back(single_landmark_temp);
 	}
+
 	return true;
 }
 
 /* Reads control data from a file.
- * @param filename Name of file containing control measurements.
- * @output True if opening and reading file was successful
- */
+* @param filename Name of file containing control measurements.
+* @output True if opening and reading file was successful
+*/
 inline bool read_control_data(std::string filename, std::vector<control_s>& position_meas) {
-
 	// Get file of position measurements:
 	std::ifstream in_file_pos(filename.c_str(),std::ifstream::in);
 	// Return if we can't open the file.
@@ -132,7 +127,6 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 
 	// Run over each single line:
 	while(getline(in_file_pos, line_pos)){
-
 		std::istringstream iss_pos(line_pos);
 
 		// Declare position values:
@@ -142,11 +136,9 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 		control_s meas;
 
 		//read data from line to values:
-
 		iss_pos >> velocity;
 		iss_pos >> yawrate;
 
-		
 		// Set values
 		meas.velocity = velocity;
 		meas.yawrate = yawrate;
@@ -154,15 +146,15 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 		// Add to list of control measurements:
 		position_meas.push_back(meas);
 	}
+
 	return true;
 }
 
 /* Reads ground truth data from a file.
- * @param filename Name of file containing ground truth.
- * @output True if opening and reading file was successful
- */
+* @param filename Name of file containing ground truth.
+* @output True if opening and reading file was successful
+*/
 inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
-
 	// Get file of position measurements:
 	std::ifstream in_file_pos(filename.c_str(),std::ifstream::in);
 	// Return if we can't open the file.
@@ -175,14 +167,13 @@ inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
 
 	// Run over each single line:
 	while(getline(in_file_pos, line_pos)){
-
 		std::istringstream iss_pos(line_pos);
 
 		// Declare position values:
 		double x, y, azimuth;
 
 		// Declare single ground truth:
-		ground_truth single_gt; 
+		ground_truth single_gt;
 
 		//read data from line to values:
 		iss_pos >> x;
@@ -197,15 +188,15 @@ inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
 		// Add to list of control measurements and ground truth:
 		gt.push_back(single_gt);
 	}
+
 	return true;
 }
 
 /* Reads landmark observation data from a file.
- * @param filename Name of file containing landmark observation measurements.
- * @output True if opening and reading file was successful
- */
+* @param filename Name of file containing landmark observation measurements.
+* @output True if opening and reading file was successful
+*/
 inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& observations) {
-
 	// Get file of landmark measurements:
 	std::ifstream in_file_obs(filename.c_str(),std::ifstream::in);
 	// Return if we can't open the file.
@@ -218,7 +209,6 @@ inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& o
 
 	// Run over each single line:
 	while(getline(in_file_obs, line_obs)){
-
 		std::istringstream iss_obs(line_obs);
 
 		// Declare position values:
@@ -238,6 +228,7 @@ inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& o
 		// Add to list of control measurements:
 		observations.push_back(meas);
 	}
+	
 	return true;
 }
 
--- src/map.h
+++ src/map.h
@@ -1,19 +1,18 @@
 /*
- * map.h
- *
- *  Created on: Dec 12, 2016
- *      Author: mufferm
- */
+* map.h
+*
+*  Created on: Dec 12, 2016
+*      Author: mufferm
+*/
 
 #ifndef MAP_H_
 #define MAP_H_
 
 class Map {
 public:
-	
-	struct single_landmark_s{
 
-		int id_i ; // Landmark ID
+	struct single_landmark_s{
+		int id_i; // Landmark ID
 		float x_f; // Landmark x-position in the map (global coordinates)
 		float y_f; // Landmark y-position in the map (global coordinates)
 	};
@@ -21,7 +20,5 @@ public:
 	std::vector<single_landmark_s> landmark_list ; // List of landmarks in the map
 
 };
-
-
 
 #endif /* MAP_H_ */
